df3b72eb88d3ea0a6ab6f534a278cc3a297b70e9
HarryChen0506 <chenwenhua0506@sina.com>
2020-03-05 18:03:56 +0800

    fix: enviroment

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,13 @@
 import sum from '../code/sum'
 
-test('adds 1 + 2 to equal 3', ( ) => {
-  expect(sum(1, 2)).toBe(3)
+describe('测试sum', () => {
+  // 这是mocha的玩法，jest可以直接兼容
+  it('adds 11 + 12 to equal 23', () => {
+    expect(sum(11, 12)).toBe(23)
+  })
+  // 这是jest的玩法，推荐用这种
+  test('adds 1 + 2 to equal 3', ( ) => {
+    expect(sum(1, 2)).toBe(3)
+  })
 })
+
